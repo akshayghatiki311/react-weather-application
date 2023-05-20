908bef8aec3d74109f9f84b71876058632a9fa48
Akshay <akshayghatiki311@gmail.com>
2023-05-21 00:46:35 +0530

    Added temperature toggle option

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,18 @@
-import './Weather.css';
+import "./Weather.css";
+import { useState } from "react";
 
 const Weather = (props) => {
+  const [tempUnit, setTempUnit] = useState("C");
+  const [temp, setTemp] = useState(props.data.temp_c);
+  const toggleTemp = async () => {
+    if (tempUnit === "C") {
+      setTempUnit("F");
+      setTemp(props.data.temp_f);
+    } else if (tempUnit === "F") {
+      setTempUnit("C");
+      setTemp(props.data.temp_c);
+    }
+  };
   return (
     <div className="output-sec">
       <div className="location">
@@ -8,7 +20,21 @@ const Weather = (props) => {
       </div>
       <img src={props.data.condition.icon} alt="" />
       <div className="sky-status">{props.data.condition.text}</div>
-      <div className="temp">Temperature : {props.data.temp_c}°C</div>
+      <div className="temp mx-5">
+        Temperature : {temp}°{tempUnit}
+      </div>
+      <div className="d-flex">
+        <label className="form-check-label mx-2">°C</label>
+        <div className="form-check form-switch" onClick={toggleTemp}>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            role="switch"
+            id="tempUnit"
+          />
+          <label className="form-check-label">°F</label>
+        </div>
+      </div>
       <div className="humidity">Humidity : {props.data.humidity}</div>
     </div>
   );
